b3ae7ed8cc3709d7f6811a35779f71dbe03abced
Khazoda <khazoda@gmail.com>
2024-04-25 14:33:05 +0100

    base skeleton + font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page-container">
+	<div class="center-container">
+		<h1>Khazoda's Mods</h1>
+		<div class="project-grid">
+			<span>one</span>
+			<span>two</span>
+			<span>three</span>
+			<span>four</span>
+			<span>five</span>
+			<span>six</span>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.page-container {
+		position: static;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.center-container {
+		position: static;
+		width: fit-content;
+		width: -moz-fit-content;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.project-grid {
+		display: grid;
+		grid-template-columns: repeat(6, 1fr);
+		grid-template-rows: 1fr;
+		grid-column-gap: 5px;
+		grid-row-gap: 0px;
+		justify-items: center;
+	}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), StaticHMR()],
+	
 });
+
+function StaticHMR() {
+	return {
+		name: 'static-hmr',
+		enforce: 'post' as const,
+		handleHotUpdate({ file, server }: any) {
+			if (file.endsWith('.css')) {
+				server.ws.send({
+					type: 'full-reload',
+					path: '*'
+				});
+			}
+		}
+	};
+}

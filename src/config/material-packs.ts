97e1d9f8cfea1623ff79c1207d1aed7fceab1ac2
June <khazoda@gmail.com>
2025-03-26 23:41:57 +0000

    add MinigameKing's ae2 bwmp

--- src/config/material-packs.ts
+++ src/config/material-packs.ts
@@ -38,5 +38,11 @@ export const MATERIAL_PACKS: MaterialPackMetadata[] = [
 		slug: 'bwmp_amethyst_minecraft',
 		category: 'vanilla-like',
 		official: true
+	},
+	{
+		slug: 'bwmp-ae2-integration',
+		required_mod_slug: 'ae2',
+		category: 'mod compatibility',
+		official: false
 	}
 ];

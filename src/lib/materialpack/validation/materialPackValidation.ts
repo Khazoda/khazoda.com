b71ad7b5b10d7799de8a5b2252ab1a7445577768
June <khazoda@gmail.com>
2025-03-28 16:12:37 +0000

    Increase name validation checks from 16/24 to 32 characters

--- src/lib/materialpack/validation/materialPackValidation.ts
+++ src/lib/materialpack/validation/materialPackValidation.ts
@@ -5,14 +5,14 @@ import { RecipeTypes } from '$lib/materialpack/types/materialpackTypes';
 export const materialPackNameSchema = z
 	.string()
 	.min(1, "Pack name can't be empty")
-	.max(16, "Pack name can't be longer than 16 characters")
+	.max(32, "Pack name can't be longer than 32 characters")
 	.transform(val => val.toLowerCase()) // Convert to lowercase
 	.transform(val => val.replace(/\s+/g, '-')) // Replace spaces with underscores
 	.transform(val => val.replace(/[^a-z0-9-]/g, '')); // Remove any non-alphanumeric characters except underscores
 
 export const modDependencySchema = z
 	.string()
-	.max(24, "Mod dependency name can't be longer than 24 characters")
+	.max(32, "Mod dependency name can't be longer than 32 characters")
 	.transform(val => val.toLowerCase())
 	.transform(val => val.replace(/\s+/g, '-'))
 	.transform(val => val.replace(/[^a-z0-9-]/g, ''));
@@ -21,7 +21,7 @@ export const modDependencySchema = z
 export const materialNameSchema = z
 	.string()
 	.min(1, "Material name can't be empty")
-	.max(16, "Material name can't be longer than 16 characters")
+	.max(32, "Material name can't be longer than 32 characters")
 	.transform(val => val.toLowerCase())
 	.transform(val => val.replace(/\s+/g, '_'))
 	.transform(val => val.replace(/[^a-z0-9_]/g, ''));

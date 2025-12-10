677707afd46c18dbbfce978cc1b6bcccfd8f6216
khazoda <khazoda@gmail.com>
2025-12-10 20:08:57 +0000

    update button banner to not be visible on site, plus some slight style changes

---
+++ src/routes/heirlooms/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import HeirloomsDescription from "$lib/mod_descriptions/heirlooms.md";
+	import MarkdownLayout from "$lib/components/MarkdownLayout.svelte";
+</script>
+
+<head>
+	<link rel="stylesheet" href="/markdown.css" />
+</head>
+
+<MarkdownLayout>
+	<HeirloomsDescription />
+</MarkdownLayout>
